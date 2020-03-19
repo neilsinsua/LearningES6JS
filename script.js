a0f8c492f1575897a36723cfae771a60f9230bac
neil sinsua <neil.sinsua@gmail.com>
2020-03-19 17:43:15 +1100

    const and let variables

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log('test');
-another TextDecoderStream;
+// Let variables:
+let i = 25;
+
+for(let i = 0; i < 6; i++) {
+  console.log(i)
+}
+
+console.log(i)
+
+// const variables:
+const i;
+
+i = 7;
+console.log(i) //error
